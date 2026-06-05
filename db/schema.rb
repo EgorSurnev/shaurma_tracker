9f64c036ba3abdf0e5958c4e0c8a139027f80b1e
Chacha-Muchacha <sdonec3@gmail.com>
2026-06-05 21:39:46 +0300

    Add nickname, Gravatar, User roles and Profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_05_155850) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_05_182616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_155850) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
